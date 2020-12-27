cc8c0f97ae37ad6bd4a9c9c58e1e87a9b222c521
lorempipsum <lootus.sander@gmail.com>
2020-12-27 22:14:32 +0000

    better image sizing for small screens

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,16 @@ module.exports = {
         path: `${__dirname}/src/data/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    // to generate markdown pages
+    `gatsby-transformer-remark`,
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-material-ui`,
