f335b7a7725320c286b956ff835c952c36d41a9b
lorempipsum <lootus.sander@gmail.com>
2021-02-23 19:51:13 +0000

    add flashy lights to cryptoButton

--- src/pages/experiments/cryptoButton.js
+++ src/pages/experiments/cryptoButton.js
@@ -2,16 +2,109 @@ import React, { useState } from 'react';
 
 import Layout from '../../components/Layout';
 
+// Generate more pleasant random colors.
+// Thanks bendc @ https://gist.github.com/bendc/76c48ce53299e6078a76
+const randomColor = () => {
+  // Why didn't importing this from an external file work?
+  'use strict';
+
+  const randomInt = (min, max) => {
+    return (
+      Math.floor(
+        Math.random() * (max - min + 1)
+      ) + min
+    );
+  };
+
+  const h = randomInt(0, 360);
+  const s = randomInt(42, 98);
+  const l = randomInt(40, 90);
+
+  return `hsl(${h},${s}%,${l}%)`;
+};
+
+const setColors = (color) => {
+  const boxes = document.getElementsByClassName(
+    'flexBox'
+  );
+  const inputColor = color;
+  const step = 20;
+
+  for (
+    let i = 0;
+    i < boxes.length;
+    i++
+  ) {
+    const timeoutLength = 20;
+    const color = inputColor?.length
+      ? inputColor
+      : randomColor();
+    setTimeout(() => {
+      boxes[
+        i
+      ].style.backgroundColor = color;
+    }, i * timeoutLength - i * i * step);
+  }
+  //.style.backgroundColor="#" + randomColor;
+};
+
+// Start or stop the party depending on whether it is party mode
+const party = (isPartyMode) => {
+  const boxes = document.getElementsByClassName(
+    'flexBox'
+  );
+  if (isPartyMode) {
+    let interval = window.setInterval(
+      () => {
+        let color = undefined;
+        if (Math.random() > 0.7) {
+          color = '#ffffff';
+        }
+        setColors(color);
+      },
+      200 + 40 * Math.random()
+    );
+    console.log(
+      `Setting interval ${interval}`
+    );
+    return interval;
+  } else {
+    console.log(
+      'party already happenin'
+    ); //for clearing all intervals
+  }
+};
+
 const Rule30 = () => {
   const [times, setTimes] = useState(8);
   const [
     overlay,
     setOverlay,
-  ] = useState({
-  });
+  ] = useState({});
+
+  const [
+    isPartyMode,
+    setIsPartyMode,
+  ] = useState(true);
+
+  const [
+    partyCubeSection,
+    setPartyCubeSection,
+  ] = useState(<div></div>);
+
+  const partyCubes = (
+    <div
+      className="gridContainer justifySpaceEvenly"
+    >
+      {[...Array(400)].map((e, i) => (
+        <div className="flexBox small" />
+      ))}
+    </div>
+  );
 
   const newOverlay = {
     position: 'fixed',
+    display: 'none',
     ZIndex: '100',
     width: '100vw',
     height: '100vh',
@@ -31,6 +124,8 @@ const Rule30 = () => {
 
   return (
     <div style={overlay}>
+      {partyCubeSection}
+
       <Layout title="Experiments - Essence Container">
         <p>
           Please note you have activated
@@ -59,8 +154,15 @@ const Rule30 = () => {
           }}
           onClick={() => {
             setTimes(times + 1);
+            party(true);
             setOverlay(newOverlay);
-            setTimeout(setOverlay(nextOverlay), 100)
+            setTimeout(
+              setOverlay(nextOverlay),
+              100
+            );
+            setPartyCubeSection(
+              partyCubes
+            );
           }}
         >
           Activate
