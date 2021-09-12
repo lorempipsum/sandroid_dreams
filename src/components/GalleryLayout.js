7828d854216b92fe28ce74dcf96feb6d4f8e680a
sandroid <maxwell.edison@gmail.com>
2021-09-12 21:00:23 +0100

    add background image via react component, not in css

--- src/components/GalleryLayout.js
+++ src/components/GalleryLayout.js
@@ -3,41 +3,31 @@ import Title from './Title';
 import ArticleLink from './ArticleLink';
 import styles from './GalleryLayout.module.css';
 
+import Delta3200BackgroundImage from '../data/images/rolls/3/000097180001.jpg';
+
 import '../styles/animations.css';
 
-const GalleryLayout = ({
-  children,
-  title,
-}) => {
+const GalleryLayout = ({ children, title }) => {
   return (
     <>
       <div className={styles.main}>
         <div className={styles.header}>
-          <Title
-            title={'sandroid.dev'}
-          />
+          <Title title={'sandroid.dev'} />
         </div>
 
         <div className={styles.content}>
-          <div
+          <img
+            src={Delta3200BackgroundImage}
             className={`${styles.heroImage} heroEntrance`}
-          >
-            {title && (
-              <div
-                className={
-                  styles.heroText
-                }
-              >
-                <h1>{title}</h1>{' '}
-              </div>
-            )}
-          </div>
-          <div
-            className={styles.divider}
-          />
-          <div className="fadeIn">
-            {children}
-          </div>
+            styles={{ background: Delta3200BackgroundImage }}
+          ></img>
+          {title && (
+            <div className={styles.heroText}>
+              <h1>{title}</h1>{' '}
+            </div>
+          )}
+          <div className={styles.divider} />
+          <div className="fadeIn">{children}</div>
         </div>
       </div>
     </>
--- src/pages/rolls/4.js
+++ src/pages/rolls/4.js
@@ -1,34 +1,25 @@
 import * as React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../../components/Layout';
-import ImageGrid from '../../components/ImageGrid';
+import GalleryLayout from '../../components/GalleryLayout';
+import GalleryImageGrid from '../../components/GalleryImageGrid';
 
 export const query = graphql`
   query Roll4ImagesQuery {
     allFile(
       filter: {
-        sourceInstanceName: {
-          eq: "images"
-        }
-        relativeDirectory: {
-          eq: "rolls/chinon_portra_400_mkIII"
-        }
+        sourceInstanceName: { eq: "images" }
+        relativeDirectory: { eq: "rolls/chinon_portra_400_mkIII" }
         ext: { eq: ".jpg" }
       }
     ) {
       edges {
         node {
           childImageSharp {
-            thumb: fluid(
-              maxWidth: 150
-              maxHeight: 150
-            ) {
+            thumb: fluid(maxWidth: 800, maxHeight: 800) {
               ...GatsbyImageSharpFluid
             }
-            full: fluid(
-              maxWidth: 2544
-            ) {
+            full: fluid(maxWidth: 2544) {
               ...GatsbyImageSharpFluid
             }
           }
@@ -40,24 +31,23 @@ export const query = graphql`
 
 const Roll = ({ data }) => {
   return (
-    <Layout title="Roll 4 - Portra 400, Chinon CP-7m.">
+    <GalleryLayout title="Roll 4 - Portra 400, Chinon CP-7m.">
       <p>
         <span>
-          The first roll from the Chinon without light leaks after taping the back window. Lots of snapshots, I really tried to fill the roll ASAP.
+          The first roll from the Chinon without light leaks after taping the
+          back window. Lots of snapshots, I really tried to fill the roll ASAP.
         </span>
       </p>
-      <ImageGrid
-        images={data.allFile.edges.map(
-          ({ node }) =>
-            node.childImageSharp
-        )}
+      <GalleryImageGrid
+        images={data.allFile.edges.map(({ node }) => node.childImageSharp)}
       />
-      {// TODO: BEFORE / AFTER Photo slider component! Show the original scan, and the edits I did to help me remember wtff I did!
+      {
+        // TODO: BEFORE / AFTER Photo slider component! Show the original scan, and the edits I did to help me remember wtff I did!
       }
       <p>
         <span></span>
       </p>
-    </Layout>
+    </GalleryLayout>
   );
 };
 
