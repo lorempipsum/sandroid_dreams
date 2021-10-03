07770169883435986278c07177fdca2f712904a2
sandroid <maxwell.edison@gmail.com>
2021-10-03 20:18:32 +0100

    add album source to gitignore. Progress the auto-album-generator

--- generate_galleries.js
+++ generate_galleries.js
@@ -12,3 +12,37 @@
  *
  **/
 var im = require('imagemagick');
+
+const path = require('path');
+
+const fs = require('fs');
+
+const albumSourceDirectoryPath = path.join(__dirname, 'ALBUM_SOURCE');
+
+const ACCEPTABLE_FILE_EXTENSIONS = ['.jpg', '.jpeg', '.png'];
+
+fs.readdir(albumSourceDirectoryPath, function (err, folders) {
+  if (err) {
+    console.log(`Encountered an error: ${err}`);
+  }
+
+  folders.forEach(function (folder) {
+    console.log(`Found folder: ${folder}`);
+
+    const folderPath = path.join(albumSourceDirectoryPath, folder);
+
+    fs.readdir(folderPath, function (err, files) {
+      if (err) {
+        console.log(`Encountered an error: ${err}`);
+      }
+
+      files.forEach(function (file) {
+        const fileNameExtension = path.extname(file).toLowerCase();
+        console.log(fileNameExtension);
+        if (ACCEPTABLE_FILE_EXTENSIONS.includes(fileNameExtension)) {
+          console.log(`Found file: ${file}`);
+        }
+      });
+    });
+  });
+});
