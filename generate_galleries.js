e6d367a54f6b0dec2ba8ad7deb5afe82c398f6b5
sandroid <maxwell.edison@gmail.com>
2021-10-05 23:13:31 +0100

    add photos

--- generate_galleries.js
+++ generate_galleries.js
@@ -3,7 +3,7 @@
  * Given:
  *    There's a bunch of jpeg files in D/Art/Photography/Somewhere
  * Do:
- *    1. Copy the files to the appropriate place
+ *    1. Copy the files to the appropriate place (./src/data/images/generatedAlbums/${ALBUM_NAME})
  *    2. Compress the files for the web (imagemagick? Maybe there's like a
  * node version of it? A way to call it through node?)
  *    3. Create src/pages/${ALBUM_NAME}.js files
@@ -20,6 +20,20 @@ const fs = require('fs');
 const albumSourceDirectoryPath = path.join(__dirname, 'ALBUM_SOURCE');
 
 const ACCEPTABLE_FILE_EXTENSIONS = ['.jpg', '.jpeg', '.png'];
+
+const copyAndResizeImage = (inputPath, outputPath) => {
+  const ImageMagickArgs = [inputPath, '-strip', '-quality', '75', outputPath];
+
+  // TODO check if file exists already. If it does, then do nothing.
+  // Else do the convee
+  return im.convert(ImageMagickArgs, function (err, stdout) {
+    console.log(`converting ${inputPath}`);
+    if (err) {
+      console.log(`error converting: ${err}`);
+      console.log(inputPath);
+    }
+  });
+};
 
 fs.readdir(albumSourceDirectoryPath, function (err, folders) {
   if (err) {
@@ -38,9 +52,21 @@ fs.readdir(albumSourceDirectoryPath, function (err, folders) {
 
       files.forEach(function (file) {
         const fileNameExtension = path.extname(file).toLowerCase();
-        console.log(fileNameExtension);
         if (ACCEPTABLE_FILE_EXTENSIONS.includes(fileNameExtension)) {
-          console.log(`Found file: ${file}`);
+          const inputPath = path.join(folderPath, file);
+          const outputPath = path.join(
+            `./src/data/images/generatedAlbums/`,
+            folder,
+            file
+          );
+          const outputFolder = path.join(
+            `./src/data/images/generatedAlbums/`,
+            folder
+          );
+          if (!fs.existsSync(outputFolder)) {
+            fs.mkdirSync(outputFolder, { recursive: true });
+          }
+          copyAndResizeImage(inputPath, outputPath);
         }
       });
     });
